796363bf3998d53f5a0c7c34273fa489710339b0
hungnt1486 <hungnt1486@gmail.com>
2020-02-28 10:13:20 +0700

    Add files via upload

---
+++ godme/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  godme
+//
+//  Created by fcsdev on 2/28/20.
+//  Copyright © 2020 godme. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ godme/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  godme
+//
+//  Created by fcsdev on 2/28/20.
+//  Copyright © 2020 godme. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
