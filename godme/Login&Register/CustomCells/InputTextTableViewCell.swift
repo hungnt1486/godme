249d0511e13cd86d5ceaac5a74e7665b75c78328
Lê Hùng <hungnt1486@gmail.com>
2020-03-04 12:39:11 +0700

    cm code

---
+++ godme/Login&Register/CustomCells/InputTextTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  InputTextTableViewCell.swift
+//  godme
+//
+//  Created by Lê Hùng on 3/4/20.
+//  Copyright © 2020 godme. All rights reserved.
+//
+
+import UIKit
+
+class InputTextTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ godme/Login&Register/RegisterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterViewController.swift
+//  godme
+//
+//  Created by Lê Hùng on 3/4/20.
+//  Copyright © 2020 godme. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
