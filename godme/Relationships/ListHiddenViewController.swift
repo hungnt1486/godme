e29a8cdbe5918302c466d242493741286755f160
Lê Hùng <hungnt1486@gmail.com>
2020-03-13 12:16:39 +0700

    cm

--- godme/Relationships/ListHiddenViewController.swift
+++ godme/Relationships/ListHiddenViewController.swift
@@ -15,6 +15,7 @@ class ListHiddenViewController: BaseViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.setupTableView()
     }
 
     func setupTableView(){
